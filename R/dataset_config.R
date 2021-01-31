3eab967cb36010e1252e0f1a0a1c946a63a6e31b
xsun28 <xsun28@gmail.com>
2021-01-31 05:16:46 +0000

    add new PBMC and ADASD datasets preprocessing

--- R/dataset_config.R
+++ R/dataset_config.R
@@ -1,10 +1,15 @@
-raw_datasets <- list(PBMC="PBMC_AllCells_withLabels.RData",
-                     pancreas=c("Muraro_pancreas.RData","Segerstolpe_pancreas.RData","Xin_pancreas.RData"))
+raw_datasets <- list(PBMC=c("PBMC_AllCells_withLabels.RData","GSE96583_batch1_3_samples.RData","GSE96583_8_Stim_Pats.RData","GSE96583_8_Ctrl_Pats.RData"),
+                     pancreas=c("Muraro_pancreas.RData","Segerstolpe_pancreas.RData","Xin_pancreas.RData"),
+                     adasd="AD_autism_data.RData",
+                     mid_brain="midbrain.RData",
+                     lung_cancer="lung_cancer.RData")
 
-datasets <- list(PBMC="PBMC_AllCells_withLabels.RDS",
+datasets <- list(PBMC=list("PBMC_AllCells_withLabels.RDS","GSE96583_batch1_3_samples.RDS","GSE96583_8_Stim_Pats.RDS","GSE96583_8_Ctrl_Pats.RDS"),
                  pancreas=list(muraro="Muraro_pancreas_clean.RDS",
                                seger="Segerstolpe_pancreas_clean.RDS",
-                               xin="Xin_pancreas_clean.RDS"))
+                               xin="Xin_pancreas_clean.RDS"),
+                 ADASD=list(AD="ADASD_AD.RDS",autism="ADASD_autism.RDS"))
+
 batch_effects_free_datasets <-list(pancreas=list(muraro="Muraro_pancreas_batch_effects_free.RDS",
                                                  seger="Muraro_pancreas_batch_effects_free.RDS",
                                                  xin="Xin_pancreas_batch_effects_free.RDS"
@@ -13,5 +18,6 @@ batch_effects_free_datasets <-list(pancreas=list(muraro="Muraro_pancreas_batch_e
 dataset.properties <- list(pancreas=list(sample_threshold=100,cell_types=c('beta','alpha','delta',
                                                                            'acinar','ductal','gamma',
                                                                            'endothelial')),
-                           PBMC=list(sample_threshold=100)
+                           PBMC=list(sample_threshold=100),
+                           ADASD=list(sample_threshold=100)
                            )
--- R/preprocess_data.R
+++ R/preprocess_data.R
@@ -3,10 +3,13 @@
 source("R/config.R")
 source("R/utils.R")
 source("R/dataset_config.R")
-preprocess_dataset <- function(dataset=c("PBMC","pancreas")) {
+preprocess_dataset <- function(dataset=c("PBMC","pancreas","midbrain","ADASD","lung_cancer")) {
   switch(dataset,
          PBMC = preprocess_PBMC(dataset),
          pancreas = preprocess_pancreas(dataset),
+         midbrain = preprocess_midbrain(dataset),
+         ADASD = preprocess_ADASD(dataset),
+         lung_cancer = preprocess_lung_cancer(dataset),
          stop("Unknown datasets")
   )  
 }
@@ -16,6 +19,7 @@ preprocess_PBMC <- function(dataset){
   require(Matrix)
   require(tidyverse)
   dataset_paths <- utils.get_dataset_paths(raw_data_home,raw_datasets[[dataset]])
+  ###load PBMC_AllCells_withLabels.RData
   load(dataset_paths[[1]])
   #data <- eval(as.name(x)) ##rename PBMC dataset to variable data
   data <- c()
@@ -28,11 +32,41 @@ preprocess_PBMC <- function(dataset){
   labels <- flatten_chr(labels)
   rm(AllCounts)
   gc(TRUE)
-  data <- utils.convert_to_SingleCellExperiment(data,rownames(data),colnames(data),tibble(label=labels),list(study='PBMC'))
+  data <- utils.convert_to_SingleCellExperiment(data,rownames(data),colnames(data),tibble(label=labels),list(study='PBMC', study_name="All_PBMC"))
   rowData(data)$EnsembleId <- purrr::map_chr(rownames(data),~str_split(.,"\\t")[[1]][[1]])
   rowData(data)$geneName <- purrr::map_chr(rownames(data), ~str_split(.,"\\t")[[1]][[2]])
-  new_dataset_path <- utils.get_dataset_paths(data_home,datasets[[dataset]])
+  new_dataset_path <- utils.get_dataset_paths(data_home,datasets[[dataset]][[1]])
   write_rds(data,new_dataset_path)
+  
+  ##load GSE96583_batch1_3_samples.RData
+  dataset_names <- load(dataset_paths[[2]])
+  sces <- map(map(dataset_names,~eval(as.name(.))), ~{colData(.)$sampleId <- colData(.)$ind
+                                                    colData(.)$label <- colData(.)$cell.type
+                                                    return(.)})
+  sces <- utils.combine_SCEdatasets(sces,if_combined=T)
+  metadata(sces) <- list(study='PBMC', study_name="GSE96583_batch1_3_samples")
+  new_dataset_path <- utils.get_dataset_paths(data_home,datasets[[dataset]][[2]])
+  write_rds(sces,new_dataset_path)
+  
+  ###load GSE96583_8_Stim_Pats.RData
+  dataset_names <- load(dataset_paths[[3]])
+  sces <- map(eval(as.name(dataset_names[[1]])), ~{colData(.)$sampleId <- colData(.)$ind
+                                                   colData(.)$label <- colData(.)$cell
+                                                   return(.)})
+  sces <- utils.combine_SCEdatasets(sces,if_combined=T)
+  metadata(sces) <- list(study='PBMC', study_name="GSE96583_8_Stim_Pats")
+  new_dataset_path <- utils.get_dataset_paths(data_home,datasets[[dataset]][[3]])
+  write_rds(sces,new_dataset_path)
+  
+  ###load GSE96583_8_Ctrl_Pats.RData
+  dataset_names <- load(dataset_paths[[4]])
+  sces <- map(eval(as.name(dataset_names[[1]])), ~{colData(.)$sampleId <- colData(.)$ind
+                                                   colData(.)$label <- colData(.)$cell
+                                                   return(.)})
+  sces <- utils.combine_SCEdatasets(sces,if_combined=T)
+  metadata(sces) <- list(study='PBMC', study_name="GSE96583_8_Ctrl_Pats")
+  new_dataset_path <- utils.get_dataset_paths(data_home,datasets[[dataset]][[4]])
+  write_rds(sces,new_dataset_path)
 }
 
 ##convert Pancreas dataset into singlecellexperiment R objects and save
@@ -58,6 +92,42 @@ preprocess_pancreas <- function(dataset){
     write_rds(data,new_dataset_path[[i]])
   }
 }
+
+###preprocess AD and autism datasets into singlecellexperiment object and save
+preprocess_ADASD <- function(dataset){
+  require(Matrix)
+  require(tidyverse)
+  dataset_paths <- utils.get_dataset_paths(raw_data_home,raw_datasets[[dataset]])
+  x <- load(dataset_paths[[1]])
+  AD <- x[1:2]
+  AD_cnt <- eval(as.name(AD[[1]])) %>%
+    as('sparseMatrix')
+  rm(list=AD[[1]])
+  gc(T)
+  AD_labels <- eval(as.name(AD[[2]]))
+  AD_data <- utils.convert_to_SingleCellExperiment(AD_cnt,rownames(AD_cnt),colnames(AD_cnt),tibble(label=AD_labels),
+                                                   list(study='ADASD',study_name="AD"))
+  rowData(AD_data)$geneName <- rownames(AD_data)
+  write_rds(AD_data,paste(data_home,"ADASD_AD.RDS"))
+  
+  autism <- x[3:4]
+  autism_cnt <- eval(as.name(autism[[1]])) %>%
+    as('sparseMatrix')
+  rm(list=autism[[1]])
+  gc(T)
+  autism_labels <- eval(as.name(autism[[2]]))
+  autism_data <- utils.convert_to_SingleCellExperiment(autism_cnt,rownames(autism_cnt),colnames(autism_cnt),tibble(label=autism_labels),
+                                                       list(study='ADASD',study_name="autism"))
+  rowData(autism_data)$geneName <- rownames(autism_data)
+  write_rds(autism_data,paste(data_home,"ADASD_autism.RDS"))
+}
+
+
+##convert PBMC dataset into singcellexperiment R objects and save
+preprocess_midbrain <- function(dataset){
+  
+}
+
 
 ##remove batch effects of datasets and saves
 preprocess.remove_batch_effects <- function(sces,file_names){
